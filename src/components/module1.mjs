6482dd48b62fb4ec1e9c6b5646201e1c3f2cde7a
miteshDemo <thakormitesh2702@gmail.com>
2025-02-18 11:09:19 +0530

    File Updated..!

---
+++ src/components/module1.mjs
@@ -0,0 +1,5 @@
+import ui, {x, y, z} from '../components/module2.mjs'
+console.log(ui);
+console.log(x);
+console.log(y);
+console.log(z);
---
+++ src/components/module2.mjs
@@ -0,0 +1,6 @@
+const x = "Yogesh";
+const y = "Hiren";
+const z = "Mitesh";
+
+
+export default x;
